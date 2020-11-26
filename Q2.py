8bd3574a0ec96703bb8e7f058e6a4f3f32b04f40
xkritti <krittamet0404@gmail.com>
2020-11-26 19:51:36 +0700

    UPDATE Q2,Q3

--- Q2.py
+++ Q2.py
@@ -3,13 +3,31 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 G.add_nodes_from(['A','B','C','D','E','F'])
-G.add_weighted_edges_from([("A","B",1),("A","D",3), ("B","C",6), ("B","D",5), ("B","E",1), ("C","E",5),("C","F",2), ("D","E",1), ("E","F",4)])
-print("Nodes of graph: ")
-print(G.nodes())
-print("Edges of graph: ")
-print(G.edges())
+G.add_weighted_edges_from([("A","B",1),
+("A","D",3), 
+("B","C",6), 
+("B","D",5), 
+("B","E",1), 
+("C","E",5),
+("C","F",2), 
+("D","E",1), 
+("E","F",4)])
 
-nx.draw_shell(G,with_labels = True,font_weight='bold')
+labels = {("A","B"):1, 
+("A","D"):3,
+("B","C"):6, 
+("B","D"):5, 
+("B","E"):1, 
+("C","E"):5, 
+("C","F"):2, 
+("D","E"):1, 
+("E","F"):4}
+pos = nx.circular_layout(G)
+edge_labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G,pos=pos, edge_labels=edge_labels)
+
+nx.draw_circular(G,with_labels = True,font_weight='bold')
 # save pix
 # plt.savefig("graphQ2.png")
+plt.text(-0.4, 1, r'Simple Graph Display', fontsize=10)
 plt.show()
--- Q3.py
+++ Q3.py
@@ -15,12 +15,22 @@ G.add_weighted_edges_from([("BKK","HKG",1000),
 ("YVR","ICN",5000),
 ("LAX","ICN",6000),
 ])
-print("Nodes of graph: ")
-print(G.nodes())
-print("Edges of graph: ")
-print(G.edges())
-
-nx.draw_shell(G,with_labels = True,font_weight='bold',edge_color="red")
+labels = {("BKK","HKG"):1000, 
+("BKK","NRT"):3000,
+("BKK","ICN"):2250, 
+("BKK","SIN"):1000, 
+("HKG","NRT"):2000, 
+("HKG","YVR"):6500, 
+("HKG","LAX"):7250, 
+("NRT","YVR"):4500, 
+("NRT","LAX"):5500, 
+("YVR","ICN"):5000, 
+("LAX","ICN"):6000, }
+pos = nx.circular_layout(G)
+nx.draw_circular(G,with_labels=True,edge_color='red')
+edge_labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G,pos=pos, edge_labels=edge_labels)
 # save pix
 # plt.savefig("graphQ2.png")
+plt.text(0, 1, r'Routes and Distances (miles)', fontsize=15)
 plt.show()
